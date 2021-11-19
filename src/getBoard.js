55628c380562c16f75cfe980e4eadaf537933384
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-11-18 19:24:11 -0500

    Address PR feedback - Use arrow notation for getBoard

--- src/getBoard.js
+++ src/getBoard.js
@@ -1,6 +1,6 @@
 import Leaderboard from './Leaderboard.js';
 
-async function getBoard() {
+const getBoard = async () => {
   try {
     return await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/iY1b10i36owa57OM6eVR/scores/')
       .then((response) => response.json())
@@ -10,6 +10,6 @@ async function getBoard() {
   } catch (e) {
     return null;
   }
-}
+};
 
 export { getBoard as default };
